895baee32aaff06b38f47080e4a9ed2e2662f22a
Pedro Perillo <pedrofperillo@gmail.com>
2023-07-19 23:18:54 -0600

    crud created

---
+++ src/routes/products/+page.server.js
@@ -0,0 +1,8 @@
+
+export async function load(event) {
+    const { fetch } = event;
+    const result = await fetch('/api/products');
+    return {
+        products: await result.json(),
+    }
+}
---
+++ src/routes/stripe/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let data;
+</script>
+
+<h1>is this working?</h1>
+<pre>
+    {JSON.stringify(data,null,4)}
+</pre>
